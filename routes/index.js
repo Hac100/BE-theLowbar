fd06283249a3980279e3734740961c5d431aa601
Phil Worthington <phil@konkura.com>
2017-10-28 22:33:46 +0100

    Added cross origin handling

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let coords=[];
+router.get('/', function (req, res, next) {
+  let coords = [];
   req.app.locals.dataStore.forEach(report => {
     //coords.push( `new google.maps.LatLng(${report.lat},${report.lon})`);
     let threatlevel = 1;
-    if(report.threatlevel === 'high') threatlevel = 3;
-    if(report.threatlevel === 'medium') threatlevel = 2;
-    
-    coords.push( `[${threatlevel},${report.lat},${report.lon}, 1]`);
+    if (report.threatlevel === 'high') threatlevel = 3;
+    if (report.threatlevel === 'medium') threatlevel = 2;
+
+    coords.push(`[${threatlevel},${report.lat},${report.lon}, 1]`);
   })
-  let coordstr =  '['+coords.join(',')+']';
+  let coordstr = '[' + coords.join(',') + ']';
 
   let alerts = analyseReports(req.app.locals.dataStore);
 
-  res.render('index', { title: 'Lowbar: lowering the threat reporting bar', coords:coordstr, reports: req.app.locals.dataStore, alerts: alerts});
+  res.render('index', { title: 'Lowbar: lowering the threat reporting bar', coords: coordstr, reports: req.app.locals.dataStore, alerts: alerts });
 });
 
 router.post('/reportthreat', reportThreat)
 
 
-function reportThreat(req, res, next){
+function reportThreat(req, res, next) {
   req.app.locals.dataStore.unshift(req.body); //add to front of array so most recent first
 
   res.status(200).send('Threat report logged');
 }
 
-function analyseReports(dataStore){
+function analyseReports(dataStore) {
   // let alerts = [];
   // alerts.push({threatlevel: 1, "lat": "53.477131", "lon": "-2.254062", type: 'Suspected bomb attack' });
   // alerts.push({threatlevel: 2, "lat": "53.477000", "lon": "-2.253000", type: 'Suspected vehicle attack' });
@@ -36,6 +36,8 @@ function analyseReports(dataStore){
   // // // We want to look for clusters of reports in a location
   let alerts = [];
   // Should really try automated clustering but for now let's take the reports in order and detect if they are close, start a new cluster if too big gap
+  let ds = dataStore;
+
   let ds = dataStore;
   let clusters = [];
   let clusterStart = ds[0];
@@ -63,22 +65,22 @@ function analyseReports(dataStore){
 }
 
 
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
   var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   var d = R * c; // Distance in km
   return d;
 }
 
 function deg2rad(deg) {
-  return deg * (Math.PI/180)
+  return deg * (Math.PI / 180)
 }
 
 module.exports = router;
