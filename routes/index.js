53d6958e6a0ada2fc7ef2b96a434d6f03aaede85
Phil Worthington <phil@konkura.com>
2017-10-28 16:07:45 +0100

    Added basic endpoint to store threat reports

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const dataStore = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lowbar: lowering the threat reporting bar', coords: '[    new google.maps.LatLng(53.477131, -2.254062),            new google.maps.LatLng(53.477, -2.254062),            new google.maps.LatLng(53.477, -2.2530),            new google.maps.LatLng(53.477131, -2.253)   ];' });
 });
+
+router.post('/reportthreat', reportThreat)
+
+
+function reportThreat(req, res, next){
+  dataStore.push(req.body);
+  console.log(dataStore)
+  res.status(200).send('Threat report logged');
+}
+
 
 module.exports = router;
