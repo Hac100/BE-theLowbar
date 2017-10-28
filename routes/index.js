bb50d6d6a845ebad1a8aadaa62a28264b9d9521c
Phil Worthington <phil@konkura.com>
2017-10-28 15:40:57 +0100

    Added basic code to display a google map and draw a polygon defined in the code, with auto refresh of whole page every 5 seconds

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Lowbar: lowering the threat reporting bar', coords: '[    new google.maps.LatLng(53.477131, -2.254062),            new google.maps.LatLng(53.477, -2.254062),            new google.maps.LatLng(53.477, -2.2530),            new google.maps.LatLng(53.477131, -2.253)   ];' });
 });
 
 module.exports = router;
