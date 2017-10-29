d5267fbf3f0660e35b043cdc4a7db82d167e98c4
Phil Worthington <phil@konkura.com>
2017-10-29 01:44:42 +0000

    Incremental update

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var reload = require('reload');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var reloadServer = reload(app);
 
-const allowCrossDomain = function(req, res, next) {
+const allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -32,18 +33,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.locals.dataStore = [];
 
+
+
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
